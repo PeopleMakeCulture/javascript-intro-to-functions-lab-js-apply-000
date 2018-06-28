6e14e5e8a4ff470abeedbdcc64ace277eb4ea24a
Jing Cao <jingcao.yale@gmail.com>
2018-06-28 13:25:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function sayHiToGrandma(string){
     return "I love you, too."
   }
 
-  else if (string == string.toLowerCase) {
+  else if (string == string.toLowerCase()) {
     return "I can\'t hear you!"
   }
 
